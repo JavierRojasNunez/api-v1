7786f3eccc1044b5a8a9524e2fb76a367130f11a
Javier Rojas <jaronu42@gmail.com>
2021-04-12 10:50:42 +0200

    Add files via upload

---
+++ routes/api.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});*/
+
+
+Route::post('/v1/create/{id?}', [App\Http\Controllers\ApiController::class, 'create']);
+Route::get('/v1/resume/{limit?}/{id?}', [App\Http\Controllers\ApiController::class, 'getResumeWithImages']);
+Route::delete('/v1/eliminar-anuncio/{id}', [App\Http\Controllers\ApiController::class, 'delete']);
+Route::put('/v1/edit/{id}', [App\Http\Controllers\ApiController::class, 'update']);
+Route::get('/v1/anuncios', [App\Http\Controllers\ApiController::class, 'getAll']);
+Route::get('/v1/anuncio/{id?}', [App\Http\Controllers\ApiController::class, 'getOne']);
+Route::get('/v1/basics/{id?}', [App\Http\Controllers\ApiController::class, 'getBasics']);
+Route::get('/v1/by/{arga}/{argb?}/{argc?}', [App\Http\Controllers\ApiController::class, 'getBy']);
+
+Route::get('/v1/nouser', [App\Http\Controllers\ApiController::class, 'noUserRegister'])->name('noUserRegister');
+Route::post('/v1/nouser', [App\Http\Controllers\ApiController::class, 'noUserRegister'])->name('noUserRegister');
+
+
+Route::group(['prefix' => 'v1/auth'], function () {
+
+    Route::post('login',  [App\Http\Controllers\AuthApiController::class, 'login'])->name('apiLogin');
+    Route::post('signup', [App\Http\Controllers\AuthApiController::class, 'signup']);    
+    Route::post('email/resend', [App\Http\Controllers\VerificationController::class, 'resend'])->name('verification.resend');
+
+    Route::group(['middleware' => 'auth:api'], function() {
+        
+        Route::group(['middleware' => 'isMailVerified:api'], function() {
+
+            Route::get('logout', [App\Http\Controllers\AuthApiController::class, 'logout']);
+            Route::get('user', [App\Http\Controllers\AuthApiController::class, 'user']);
+            
+            Route::get('resume/{limit?}/{id?}', [App\Http\Controllers\ApiController::class, 'getResumeWithImages']);
+            Route::post('create/{id?}', [App\Http\Controllers\ApiController::class, 'create']);
+            Route::post('update/{id?}', [App\Http\Controllers\ApiController::class, 'create']);
+            Route::delete('eliminar-anuncio/{id}', [App\Http\Controllers\ApiController::class, 'delete']);        
+            Route::get('anuncios', [App\Http\Controllers\ApiController::class, 'getAll']);
+            Route::get('anuncio/{id?}', [App\Http\Controllers\ApiController::class, 'getOne']);
+            Route::get('basics/{id?}', [App\Http\Controllers\ApiController::class, 'getBasics']);
+            Route::get('by/{arga}/{argb?}/{argc?}', [App\Http\Controllers\ApiController::class, 'getBy']);
+            Route::delete('delete-image/{id}', [App\Http\Controllers\ApiController::class, 'deleteImage']);
+            Route::post('upload-images/{id}', [App\Http\Controllers\ApiController::class, 'uploadImage']);
+
+        });  
+
+    });
+});
---
+++ routes/web.php
@@ -0,0 +1,50 @@
+<?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+//Auth::routes(['verify' => true]);
+Auth::routes();
+
+//rutas para la verificacion de email 
+Route::get('email/verify/{id}/{hash}', [App\Http\Controllers\VerificationController::class, 'verify'])->name('verification.verify');  
+
+
+
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::post('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/confirmation', [App\Http\Controllers\HomeController::class, 'redirectRegister']);
+//anuncios
+//Route::get('/nuevo-anuncio', [App\Http\Controllers\AnuncioController::class, 'create'])->name('create.anounce');
+Route::get('/nuevo-anuncio/{type?}', [App\Http\Controllers\AnuncioController::class, 'create'])->name('create.anounce')->middleware(['auth', 'verified' ]);
+Route::post('/publicar', [App\Http\Controllers\AnuncioController::class, 'save'])->name('save.anounce')->middleware(['auth', 'verified' ]);
+Route::get('/mis-anuncios', [App\Http\Controllers\AnuncioController::class, 'getAnounces'])->name('my.anounce')->middleware(['auth', 'verified' ]);
+Route::get('/editar-anuncio/{id}/{type?}', [App\Http\Controllers\AnuncioController::class, 'edit'])->name('edit.anounce')->middleware(['auth', 'verified' ]);
+Route::get('/eliminar-anuncio/{id}', [App\Http\Controllers\AnuncioController::class, 'delete'])->name('delete.anounce')->middleware(['auth', 'verified' ]);
+Route::get('/detalles/{anounce_id}', [App\Http\Controllers\HomeController::class, 'detail'])->name('detail.anounce');
+
+// imagenes de los anuncios
+Route::get('/eliminar-imagenes/{id}/{anounce_id}', [App\Http\Controllers\ImagenController::class, 'deleteImage'])->name('delete.image')->middleware(['auth', 'verified']);
+Route::get('/editar-imagenes/{id}/{type?}', [App\Http\Controllers\ImagenController::class, 'editImages'])->name('edit.images')->middleware(['auth', 'verified']);
+Route::get('/anounces/{id?}/{filename?}',  [App\Http\Controllers\ImagenController::class, 'getImage'])->name('image.file');
+Route::post('/guardar/imagenes', [App\Http\Controllers\ImagenController::class, 'saveImages'])->name('save.images')->middleware(['auth', 'verified']);
+//ciudades y provincias
+Route::get('/cities/{city_id}', [App\Http\Controllers\CitiesController::class, 'getCities']);
+Route::get('/adress/{char}/{province_id}', [App\Http\Controllers\CitiesController::class, 'getAdress']);
+
+//consumo api
+Route::get('/api-anuncios/{id?}', [App\Http\Controllers\GetApiDataController::class, 'getAll'])->middleware(['auth', 'verified']);
+
+
